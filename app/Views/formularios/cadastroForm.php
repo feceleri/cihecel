32d1cc8c302da58f1299453b172030cb75280a46
Matheus Bombassei <matheusbombassei@outlook.com>
2022-07-21 13:26:41 -0300

    Inclusão do formulário de cadastro

---
+++ app/Views/formularios/cadastroForm.php
@@ -0,0 +1,123 @@
+
+            <div class="row align-items-center" style="height:80vh">
+                <div class="col-md-12">
+                    <div class="card-box">
+                        <h4 class="card-title">Formulário de cadastro</h4>
+                        <h4 class="card-title">Informações Pessoais</h4>
+                        <form action="#">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group row">
+                                        <label for="nomeCompleto" class="col-md-3 col-form-label">Nome Completo</label>
+                                        <div class="col-md-9">
+                                            <input type="text" class="form-control" id="nomeCompleto">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="cpf" class="col-md-3 col-form-label">CPF</label>
+                                        <div class="col-md-9">
+                                            <input type="text" class="form-control" id="cpf" maxlength="14">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="sexo" class="col-md-3 col-form-label">Sexo</label>
+                                        <div class="col-md-9">
+                                            <div class="form-check form-check-inline">
+                                                <input class="form-check-input" type="radio" name="sexo" id="sexoMasculino" value="option1" checked>
+                                                <label class="form-check-label" for="sexoMasculino">
+                                                Masculino
+                                                </label>
+                                            </div>
+                                            <div class="form-check form-check-inline">
+                                                <input class="form-check-input" type="radio" name="sexo" id="sexoFeminino" value="option2">
+                                                <label class="form-check-label" for="sexoFeminino">
+                                                Feminino
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="dtNasc" class="col-md-3 col-form-label">Data de Nascimento</label>
+                                        <div class="col-md-4">
+                                            <input type="date" class="form-control" id="dtNasc">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group row">
+                                        <label for="nomeDaMae" class="col-md-3 col-form-label">Nome da Mãe</label>
+                                        <div class="col-md-9">
+                                            <input type="text" class="form-control" id="nomeDaMae">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="rg" class="col-md-3 col-form-label">RG</label>
+                                        <div class="col-md-9">
+                                            <input type="text" class="form-control" id="rg">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="tel1" class="col-md-3 col-form-label">Telefone 1</label>
+                                        <div class="col-md-9">
+                                            <input type="text" class="form-control" id="tel1" maxlength="15">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="tel2" class="col-md-3 col-form-label">Telefone 2</label>
+                                        <div class="col-md-9">
+                                            <input type="text" class="form-control" id="tel2" maxlength="15">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <h4 class="card-title">Endereço</h4>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group row">
+                                        <label for="cep" class="col-md-3 col-form-label">CEP</label>
+                                        <div class="col-md-9">
+                                            <input type="text" class="form-control" id="cep" maxlength="9">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="logradouro" class="col-md-3 col-form-label">Logradouro</label>
+                                        <div class="col-md-9">
+                                            <input type="text" class="form-control" id="logradouro">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="numero" class="col-md-3 col-form-label">Número</label>
+                                        <div class="col-md-4">
+                                            <input type="text" class="form-control" id="numero">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group row">
+                                        <label for="complemento" class="col-md-3 col-form-label">Complemento</label>
+                                        <div class="col-md-9">
+                                            <input type="text" class="form-control" id="complemento">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="localidade" class="col-md-3 col-form-label">Cidade</label>
+                                        <div class="col-md-9">
+                                            <input type="text" class="form-control" id="localidade">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="bairro" class="col-md-3 col-form-label">Bairro</label>
+                                        <div class="col-md-9">
+                                            <input type="text" class="form-control" id="bairro">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="text-right">
+                                <button type="submit" class="btn btn-primary" id="cadastrar">Cadastrar</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
--- app/Views/layout/cadastro.php
+++ app/Views/layout/cadastro.php
@@ -2,4 +2,75 @@
 
 <?= $this->section('conteudo') ?>
 
+<?= $this->include('formularios/cadastroForm.php')?>
+
+<?= $this->endSection() ?>
+
+<?= $this->section('script') ?>
+<!-- Script CEP -->
+<script>
+    const cep = document.querySelector("#cep")
+
+const mostraDados = (resultado)=>{
+    for(const campo in resultado){
+        if(document.querySelector("#"+campo)){
+            document.querySelector("#"+campo).value = resultado[campo]
+        }
+    }
+}
+
+
+
+
+cep.addEventListener("blur",(e)=>{
+    let search = cep.value.replace("-","")
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'default'
+    }
+
+    fetch(`https://viacep.com.br/ws/${search}/json/`, options)
+    .then(response =>{ response.json()
+        .then( dados => mostraDados(dados))
+    })
+    .catch(e => console.log('Deu Erro: '+ e,message))
+})
+
+function mascaraCPF(cpf) {
+            cpf = cpf.replace(/\D/g, "").slice(0, 11);
+            cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2");
+            cpf = cpf.replace(/^(\d{3}\.\d{3})(\d)/, "$1.$2");
+            cpf = cpf.replace(/(.{11})(\d)/, "$1-$2");
+            return cpf;
+        } // 12345678910 -> 123.456.789-10
+
+        function mascaraPhone(cel, max = 11) { // max = 10 para telefone fixo
+            cel = cel.replace(/\D/g, "").slice(0, max);
+            cel = cel.replace(/^(\d{2})(\d+)/, "($1) $2");
+            cel = cel.replace(/(\b\d+)(\d{4})$/, "$1-$2");
+            return cel;
+        } // 12345678910 -> (12) 34567-8910
+      
+        /* EXEMPLO  DE USO */
+        let cpfInput = document.querySelector('#cpf');
+        let tel1Input = document.querySelector('#tel1');
+        let tel2Input = document.querySelector('#tel2');
+        let cepInput = document.querySelector('#cep');
+
+        cpfInput.addEventListener('keyup', function() {
+            cpfInput.value = mascaraCPF(cpfInput.value);
+        });
+
+        tel1Input.addEventListener('keyup', function() {
+            tel1Input.value = mascaraPhone(tel1Input.value)
+        });  
+        tel2Input.addEventListener('keyup', function() {
+            tel2Input.value = mascaraPhone(tel2Input.value)
+        });
+
+        cepInput.addEventListener('keyup', function() {
+            cepInput.value = mascaraCEP(cepInput.value);
+        });
+</script>
 <?= $this->endSection() ?>
